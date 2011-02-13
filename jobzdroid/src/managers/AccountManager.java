7b1deb3cabef14106d720f7cc6db83c3fe2eff02
luolw8026@gmail.com <luolw8026@gmail.com@98e01662-c3e2-1594-295b-da2b039ece72>
2011-02-13 22:41:47 +0000

    user registration is now functional (without email verification)

--- jobzdroid/src/managers/AccountManager.java
+++ jobzdroid/src/managers/AccountManager.java
@@ -1,5 +1,34 @@
 package managers;
 
+/***
+ * Manages account related actions, and calls DBManager to perform DB operations
+ */
 public class AccountManager {
-
+	private DBManager dbManager;
+	
+	public AccountManager(){
+		dbManager = new DBManager();
+	}
+	
+	/***
+	 * Checks whether the given primary email address is unique
+	 * @param email email address to be checked
+	 * @return boolean indicating whether the email address is unique
+	 */
+	public boolean isUniqueEmailAddr(String email){
+		return dbManager.isUniqueEmailAddr(email);
+	}
+	
+	/***
+	 * Creates a new account with the given email, password, account type and person/company name
+	 * @param email Primary email
+	 * @param password User password
+	 * @param accType Account type
+	 * @param name Person/Company name
+	 * @return boolean indicating whether account was successfully created
+	 */
+	public boolean createAccount(String email, String password, String accType, String name){
+		return dbManager.createAccount(email, password, accType, name);
+	}
+	
 }
