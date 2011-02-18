c8b4fcb665078b6e4d9102cabdc31c3d49adb69b
abigrabit <abigrabit@98e01662-c3e2-1594-295b-da2b039ece72>
2011-02-18 08:54:32 +0000

    Harry: User Access Backend V1

---
+++ trunk/JobzDroid/WebContent/scripts/accountAction.js
@@ -0,0 +1,58 @@
+/*****************************************************************************************************
+ * 					LOG IN FUNCTION
+ ****************************************************************************************************/
+function userLoginRequest()//TODO Recover lightbox element
+{
+//	$("#loginBox").hide();
+//	openbox("sign-inLoading",'',1);
+	var userName = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+	
+	if (window.XMLHttpRequest)
+	  {// code for IE7+, Firefox, Chrome, Opera, Safari
+	  xmlhttp=new XMLHttpRequest();
+	  }
+	else
+	  {// code for IE6, IE5
+	  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	  }
+	  
+	xmlhttp.onreadystatechange=function(){
+	  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+	    {
+		    //Gets sessionKey and prints it to div
+			var sessionKey = (xmlhttp.responseXML.getElementsByTagName("userCred")[0]).childNodes[0].nodeValue;
+			var userID = (xmlhttp.responseXML.getElementsByTagName("userID")[0]).childNodes[0].nodeValue;
+			
+		    if( sessionKey != "null" ) 
+			    { 
+					var responseText = "<h2>User " + username + "logged in with SESSION KEY: ";
+										responseText += sessionKey  + "</h2>";		
+					
+					document.getElementById("myDiv").innerHTML=responseText;
+					document.getElementById("sessKey").value = sessionKey;
+					document.getElementById("name").value = userName;
+					document.getElementById("submitUserID").value = userID;
+					//document.getElementById("close").submit();
+					//$("#loginBox").remove();
+		    	}
+		    else
+		    	{
+			    	//TODO: implement error handling
+			    	alert("Login Failed");
+//		    		closePopup("sign-inLoading");
+			    	document.getElementById("myDiv").innerHTML="<h2>Login Failed!</h2>";
+//			    	$("#loginBox").show();
+		    	}
+	    }
+	  };
+	  
+	var Params = "userName=" + userName + "&password=" + password;
+
+	//send the parameters to the servlet with POST
+	xmlhttp.open("POST","../ServletAccount" ,true);
+	xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xmlhttp.send(Params);
+
+
+}
