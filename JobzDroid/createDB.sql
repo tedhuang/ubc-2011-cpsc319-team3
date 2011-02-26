bf5feab1919243c9470b6498331dfee4967fa135
luolw8026@gmail.com <luolw8026@gmail.com@98e01662-c3e2-1594-295b-da2b039ece72>
2011-02-26 02:15:18 +0000

    loading config file completed

--- JobzDroid/createDB.sql
+++ JobzDroid/createDB.sql
@@ -1,10 +1,32 @@
 CREATE DATABASE IF NOT EXISTS jobzdroid;
 USE jobzdroid;
 
-DROP TABLE IF EXISTS TableEmailVerification;
-CREATE TABLE TableEmailVerification (
-    idEmailVerification       CHAR(36) NOT NULL PRIMARY KEY,
+DROP TABLE IF EXISTS tableEmailVerification;
+CREATE TABLE tableEmailVerification (
+    idEmailVerification     CHAR(36) NOT NULL PRIMARY KEY,
     idAccount       		INTEGER NOT NULL, 
-    expiryTime     		BIGINT NOT NULL,
-    CONSTRAINT FOREIGN KEY (`idAccount`) REFERENCES `TableAccount` (`idAccount`)
+    expiryTime     			BIGINT NOT NULL,
+    emailPending			VARCHAR(254) DEFAULT NULL,
+    CONSTRAINT FOREIGN KEY (`idAccount`) REFERENCES `tableAccount` (`idAccount`)
 );
+
+DROP TABLE IF EXISTS tableLocationJobAd;
+CREATE TABLE tableLocationJobAd (
+ 	idJobAd					INT NOT NULL,
+  	location				VARCHAR(45) NOT NULL,
+  	longitude				DOUBLE DEFAULT NULL ,
+  	latitude				DOUBLE DEFAULT NULL ,
+  	PRIMARY KEY(idJobAd, location),
+	CONSTRAINT FOREIGN KEY (`idJobAd`) REFERENCES `tableJobAd` (`idJobAd`) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+DROP TABLE IF EXISTS tableLocationProfile;
+CREATE TABLE tableLocationProfile (
+ 	idAccount				INT NOT NULL,
+  	location				VARCHAR(45) NOT NULL,
+  	longitude				DOUBLE DEFAULT NULL ,
+  	latitude				DOUBLE DEFAULT NULL ,
+  	PRIMARY KEY(idAccount, location),
+	CONSTRAINT FOREIGN KEY (`idAccount`) REFERENCES `tableAccount` (`idAccount`) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
