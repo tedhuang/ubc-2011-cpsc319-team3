b45778e9dcc31abe9df0f07d876d6b2280d327bd
martinku86 <martinku86@98e01662-c3e2-1594-295b-da2b039ece72>
2011-03-08 21:29:56 +0000

    * Changed system email to jobzdroid email * Aligned code :) - Martin

--- trunk/JobzDroid/src/managers/SystemManager.java
+++ trunk/JobzDroid/src/managers/SystemManager.java
@@ -16,33 +16,34 @@ public class SystemManager {
 	/***
 	 * Global system variables with their default values. Values are overwritten after loading the configuration file.
 	 */
-	public static long sessionRenewPeriodAfterExpiry = 15 * 60 * 1000;				// default 15 mins
-	public static long expiryTimeSession = 1 * 60 * 60 * 1000; 						// default 1 hour
-	public static long expiryTimeEmailVerification = 60 * 60 * 1000;				// default 60 minutes
-	public static long expiryTimeForgetPasswordReset = 60 * 60 * 1000;  			// default 60 minutes
-	public static long expiryTimePendingAccount = 60 * 60 * 1000;  					// default 60 minutes
-	public static long timeBeforeRemovingExpiredInactiveJobAds = 7 * 24 * 60 * 60 * 1000;	// default 7 days
-	public static boolean autoRemoveExpiredInactiveJobAds = true;					// default auto remove expired inactive job ads
-	public static long timeIntervalAutomatedTasks = 10 * 60 * 1000;					// default 10 minutes
+	public static long 		sessionRenewPeriodAfterExpiry 			= 15 * 60 * 1000;				// default 15 mins
+	public static long 		expiryTimeSession 						= 1 * 60 * 60 * 1000; 			// default 1 hour
+	public static long 		expiryTimeEmailVerification 			= 60 * 60 * 1000;				// default 60 minutes
+	public static long 		expiryTimeForgetPasswordReset 			= 60 * 60 * 1000;  				// default 60 minutes
+	public static long 		expiryTimePendingAccount 				= 60 * 60 * 1000;  				// default 60 minutes
+	public static long 		timeBeforeRemovingExpiredInactiveJobAds = 7 * 24 * 60 * 60 * 1000;		// default 7 days
+	public static boolean 	autoRemoveExpiredInactiveJobAds 		= true;							// default auto remove expired inactive job ads
+	public static long 		timeIntervalAutomatedTasks 				= 10 * 60 * 1000;				// default 10 minutes
+	//TODO add maximum expiry time for each value
 	
 	// default string patterns
-	public static String emailPattern = "^[_A-Za-z0-9-\\.]+@[_A-Za-z0-9-\\.]+(\\.[A-Za-z]{2,})$";
-	public static String pwPattern = "^[_A-Za-z0-9-\\.]{5,15}$";
-	public static String namePattern = "^[_A-Za-z0-9-\\.]+$";
-	public static String phonePattern = "^\\(?(\\d{3})\\)?[- ]?(\\d{3})[- ]?(\\d{4})$";
+	public static String emailPattern 	= "^[_A-Za-z0-9-\\.]+@[_A-Za-z0-9-\\.]+(\\.[A-Za-z]{2,})$";
+	public static String pwPattern 		= "^[_A-Za-z0-9-\\.]{5,15}$";
+	public static String namePattern 	= "^[_A-Za-z0-9-\\.]+$";
+	public static String phonePattern 	= "^\\(?(\\d{3})\\)?[- ]?(\\d{3})[- ]?(\\d{4})$";
 	
 	// DB connection variables
-	public static String dbDriver = "com.mysql.jdbc.Driver";
-	public static String dbURL = "jdbc:mysql://www.db4free.net:3306/dbjobzdriod";
-	public static String dbUser = "blitzcriegteam";
-	public static String dbPassword = "cs319team3";
+	public static String dbDriver 	= "com.mysql.jdbc.Driver";
+	public static String dbURL 		= "jdbc:mysql://www.db4free.net:3306/dbjobzdriod";
+	public static String dbUser 	= "blitzcriegteam";
+	public static String dbPassword	= "cs319team3";
 	public static int maxDBConnectionPoolSize = 0;									// 0 means no limit
 	
 	// system email variables
-	public static String systemEmailAddress = "craigsbayauctionhouse@gmail.com";
-	public static String systemEmailPw = "craigsbayrocks";
-	public static String systemEmailSMTPHost = "smtp.gmail.com";
-	public static String systemEmailPort = "465";
+	public static String systemEmailAddress		= "jobzdroid@gmail.com";
+	public static String systemEmailPw			= "cs319CS#!(";
+	public static String systemEmailSMTPHost	= "smtp.gmail.com";
+	public static String systemEmailPort		= "465";
 	/***********************************************************************************************************************/
 	
 	// singleton instance
@@ -74,25 +75,25 @@ public class SystemManager {
 			return;
 	    }
 		// update system variables
-		dbDriver = config.getProperty("dbDriver");
-		dbURL = config.getProperty("dbURL");
-		dbUser = config.getProperty("dbUser");
-		dbPassword = config.getProperty("dbPassword");
-		emailPattern = config.getProperty("emailPattern");
-		pwPattern = config.getProperty("pwPattern");
-		namePattern = config.getProperty("namePattern");	
-		phonePattern = config.getProperty("phonePattern");
-		systemEmailAddress = config.getProperty("systemEmailAddress");
-		systemEmailPw = config.getProperty("systemEmailPw");
+		dbDriver			= config.getProperty("dbDriver");
+		dbURL				= config.getProperty("dbURL");
+		dbUser 				= config.getProperty("dbUser");
+		dbPassword 			= config.getProperty("dbPassword");
+		emailPattern 		= config.getProperty("emailPattern");
+		pwPattern 			= config.getProperty("pwPattern");
+		namePattern 		= config.getProperty("namePattern");	
+		phonePattern 		= config.getProperty("phonePattern");
+		systemEmailAddress 	= config.getProperty("systemEmailAddress");
+		systemEmailPw 		= config.getProperty("systemEmailPw");
 		systemEmailSMTPHost = config.getProperty("systemEmailSMTPHost");
-		systemEmailPort = config.getProperty("systemEmailPort");
+		systemEmailPort 	= config.getProperty("systemEmailPort");
 		try{
-			sessionRenewPeriodAfterExpiry = Long.parseLong(config.getProperty("sessionRenewPeriodAfterExpiry").trim()) * 60 * 1000;	// unit: minutes -> millisecond
-			expiryTimeSession = Long.parseLong(config.getProperty("expiryTimeSession").trim()) * 60 * 1000;
-			expiryTimeEmailVerification = Long.parseLong(config.getProperty("expiryTimeEmailVerification").trim()) * 60 * 1000;
-			expiryTimeForgetPasswordReset = Long.parseLong(config.getProperty("expiryTimeForgetPasswordReset").trim()) * 60 * 1000;
-			expiryTimePendingAccount = Long.parseLong(config.getProperty("expiryTimePendingAccount").trim()) * 60 * 1000;
-			timeIntervalAutomatedTasks = Long.parseLong(config.getProperty("timeIntervalAutomatedTasks").trim()) * 60 * 1000;
+			sessionRenewPeriodAfterExpiry 	= Long.parseLong(config.getProperty("sessionRenewPeriodAfterExpiry").trim()) * 60 * 1000;	// unit: minutes -> millisecond
+			expiryTimeSession 				= Long.parseLong(config.getProperty("expiryTimeSession").trim()) * 60 * 1000;
+			expiryTimeEmailVerification 	= Long.parseLong(config.getProperty("expiryTimeEmailVerification").trim()) * 60 * 1000;
+			expiryTimeForgetPasswordReset 	= Long.parseLong(config.getProperty("expiryTimeForgetPasswordReset").trim()) * 60 * 1000;
+			expiryTimePendingAccount 		= Long.parseLong(config.getProperty("expiryTimePendingAccount").trim()) * 60 * 1000;
+			timeIntervalAutomatedTasks 		= Long.parseLong(config.getProperty("timeIntervalAutomatedTasks").trim()) * 60 * 1000;
 			timeBeforeRemovingExpiredInactiveJobAds =
 				Long.parseLong(config.getProperty("timeBeforeRemovingExpiredInactiveJobAds").trim()) * 24 * 3600 * 1000;			// unit: day -> millisecond
 						
