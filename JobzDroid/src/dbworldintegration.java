36e69b38129824238a8b11f5f2937156cb7b58e6
edwinccn <edwinccn@98e01662-c3e2-1594-295b-da2b039ece72>
2011-03-17 01:27:15 +0000

    dbworldintegration.java  (Edwin C.)
    -Improved code, commented, and refractored-took out nonfunctional functions.
    	*Backup of old code in local computer
    -Certain parts still in test phase but they work:
    	*All email messages (including non-DBWorld related) are read. Function to filter out DBWorld & Job Posting emails to read
    	 have been successfully tested before
    	*Create JobAd: prompt comes up asking user if they want to create job adv. with current email message
    	*Delete Message: prompt comes up asking user if they want to delete current email message 
    
    -Need to do:
    	*Set up code to run when Tomcat starts
    	*Read inbox every time interval (1hr?)
    	*Migrate to official JobzDroid email system
    	
    	
    -Edwin C.

--- JobzDroid/src/dbworldintegration.java
+++ JobzDroid/src/dbworldintegration.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.sql.Connection;
@@ -10,23 +11,20 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Enumeration;
 import java.util.Properties;
 
 import javax.mail.BodyPart;
 import javax.mail.Flags;
 import javax.mail.Folder;
+import javax.mail.Header;
 import javax.mail.Message;
 import javax.mail.Multipart;
 import javax.mail.Part;
 import javax.mail.Session;
 import javax.mail.Store;
 
-import org.htmlparser.Node;
-import org.htmlparser.Parser;
-import org.htmlparser.filters.RegexFilter;
-import org.htmlparser.filters.TagNameFilter;
-import org.htmlparser.util.NodeList;
-import org.jsoup.Jsoup;
+import org.apache.commons.io.IOUtils;
 
 import classes.Utility;
 
@@ -35,46 +33,16 @@ import managers.DBManager;
 import sun.net.www.URLConnection;
 
 
-/**
-public class dbworldintegration {
-    public static void main(String[] args) throws Exception {
-
-
-	String inputLine;
-	inputLine= Jsoup.connect("http://www.cs.wisc.edu/dbworld/browse.html").get().html();
-	Parser parser = new Parser();
-	parser.setInputHTML(inputLine);
-	parser.setEncoding("UTF-8");
-	NodeList n1 = parser.parse(null);
-	NodeList trs = n1.extractAllNodesThatMatch(new TagNameFilter("tr"),true);
-	
-	for(int i=1;i<=10;i++) {
-	    NodeList nodes = trs.elementAt(i).getChildren();
-	    NodeList tds  = nodes.extractAllNodesThatMatch(new TagNameFilter("td"),true);
-
-	    NodeList annNode = tds.elementAt(1).getChildren();
-	    //Do stuff with jobAnnNode
-	    String annNodeToString = annNode.asString();
-	    if(annNodeToString.contains("conf. ann."))
-	    {
-	    	System.out.println(tds.asString());
-	    	System.out.println(tds.elementAt(3).getChildren().asString());
-	    }
-	    
-	} 
-    }}
-**/
-
 //ToDo:
 //(1.)	Program runs every time server is running. <--Not done
-//(2a.)	Method 1: MessageCountListener-when inbox gets new email invoke method to read new email OR <-- Omit
-//(2b.)	OR Method 2: Check inbox every time period  <--Not Done
-//(3.)	Check message subject heading, find this heading in the website and see if this dbworld news is a "job ann." news. <--Done
-//(4.)	If so, update to database. <-- Done
+//(2.)	Check inbox every time period (1hr?) <--Not Done
+//(3a.)	Check message subject heading-is this from DBWorld? <-- Done
+//(3b.) Check thru headers-is this of Job Annoucement? <-- Done
+//(4.)	If so, create Job Ad with message and update to database. <-- Done
 //(5.)	Delete the email <-- Done
 //(6.)	Check next email  <-- Done
 
-//(7.) Test if every sequence works <-- Not Done
+//(7.) Test if every sequence works <-- Not Done-need to test (1.) and (2.)
 
 public class dbworldintegration {
 	private DBManager dbManager;
@@ -103,7 +71,7 @@ public class dbworldintegration {
  * @param: password - The password of the email account
  * 
  */
-	public void emailParse(String username,String password) {
+	protected void emailParse(String username,String password) {
 		
 		//Setup to connect to JobzDroid database. Create connection.
 		Connection conn = dbManager.getConnection();
@@ -133,79 +101,109 @@ public class dbworldintegration {
 			inbox.open(Folder.READ_WRITE);
 			Message[] msg = inbox.getMessages();
 			
-			//Create statement
+			//Create statement to pass sql queries.
 			stmt = conn.createStatement();
         
-			//Setup to connect to the DBWorld mailing list website, and to parse all relevant data
-			//from the HTML table that lists DBWorld postings (job ann., conf ann., journal ann. etc.)
-			String inputLine;
-			inputLine= Jsoup.connect("http://www.cs.wisc.edu/dbworld/browse.html").get().html();
-			Parser parser = new Parser();
-			parser.setInputHTML(inputLine);
-			parser.setEncoding("UTF-8");
-			NodeList n1 = parser.parse(null);
-			NodeList trs = n1.extractAllNodesThatMatch(new TagNameFilter("tr"),true);
-        
 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Total number of messages in INBOX: "+msg.length);
 			for (int i = msg.length-1; i >= 0; i--) 
 			{
 				String getReplyTo = msg[i].getReplyTo()[0].toString();	
 				String getSubject = msg[i].getSubject();
 				getSubject = getSubject+" ";         
-				//invoke searchDBWorldAnnPostings() method, and return boolean to var isJobAnn;
-				boolean isJobAnn = searchDBWorldAnnPosting(trs, getSubject);
- 
+				
+				
 				// if email is from DBWorld mailing list, and isJobAnn:email posting is a job announcement, 
 				// then take information from email and create new JobzDroid Job Ad. with it.
-				if(getReplyTo.contentEquals("dbworld_owner@yahoo.com") && isJobAnn)
-				{	  
-					String from = msg[i].getFrom()[0].toString();
-					String replyTo = msg[i].getReplyTo()[0].toString();
-					String subject = msg[i].getSubject().toString();
-					String allHeaders = msg[i].getAllHeaders().toString();
-					Date sentDate = msg[i].getSentDate();					
-					Calendar cal = Calendar.getInstance();
-					cal.setTime(sentDate);
-					long millisSentDate = -1;
-					millisSentDate = cal.getTimeInMillis();
-					String message = msg [i].getInputStream().toString();
-					/**
-					InputStream stream = msg[i].getInputStream();
-					while (stream.available() != 0) {
-						message = stream.toString();
-					}**/
+				
+				// testCode: set conditional statement to always true so that all email are read (including
+				// test messages)
+				if(/*getReplyTo.contentEquals("dbworld_owner@yahoo.com")*/ true)
+				{	
+					boolean isJobAnn= false;
 					
-					createJobAdvertisementWithEmail(stmt, subject, message, millisSentDate/**,expiryDate**/);
+			        Enumeration headers = msg[i].getAllHeaders();
+					isJobAnn = checkHeaderForJobAnn(headers);
 					
-					//Test code-print message header and body if success
-					System.out.println("From : " + from);
-					System.out.println("Reply to : "+ replyTo);
-					System.out.println("Subject : " + subject);
-					System.out.println("Date : " + sentDate);
-					System.out.println("All Headers : " + allHeaders);        	  
-					System.out.print(message);
-
-					System.out.println();
-					System.out.println("-----------------------------------------------------");
-				}
-         
+					//testCode: set isJobAnn to true so that all DBWorld posts are read instead
+					isJobAnn = true;
+					
+					if(isJobAnn)
+					{						
+						String from = msg[i].getFrom()[0].toString(); //from header
+						String replyTo = msg[i].getReplyTo()[0].toString(); //reply-to header
+						String subject = msg[i].getSubject().toString(); //subject header
+						
+						Date sentDate = msg[i].getSentDate(); //date-sent header				
+						Calendar cal = Calendar.getInstance();
+						cal.setTime(sentDate);
+						long millisSentDate = -1; //date-sent header set to milliseconds
+						millisSentDate = cal.getTimeInMillis();
+												
+						String message = null; //message body
+						InputStream stream = msg[i].getInputStream(); 
+						while(stream.available()!= 0) {
+							StringWriter writer = new StringWriter();
+							IOUtils.copy(stream, writer);
+							message = writer.toString();
+						}
+						
+						//Test code-print message headers and body if success
+						System.out.println("From:" + from);
+						System.out.println("Reply to:"+ replyTo);
+						System.out.println("Subject:" + subject);
+						System.out.println("Date:" + sentDate);
+						System.out.println("Date in millisec:" + millisSentDate);						
+						System.out.print(message);
+						System.out.println();
+						System.out.println("-----------------------------------------------------");
+						
+						//Test Code: ask user if they want to create Job Ad with current email message.
+						System.out.println("Do you want to create a Job Ad. with this message? - " + subject);
+						String prompt = reader.readLine();
+						if("Y".equalsIgnoreCase(prompt)){
+							createJobAdvertisementWithEmail(stmt, subject, message, millisSentDate);
+						}
+						
+					}/**end of inner if() block **/						
+					else
+						System.out.println("This message is not a job announcement. Message will not be read.");
+					
+				}/**end of first outer if() block**/
+				else
+					System.out.println("This message is not DBWorld mailing list material. Message will not be read.");
+					
+				
 				//Delete the read email message after. 
-				//Test code-prompt user if they want to delete.
+				//Test code-prompt user if they want to delete current email message.
 				System.out.println("Do you want to delete the message? -- " + msg[i].getSubject());
 				String line = reader.readLine();
 				if("Y".equalsIgnoreCase(line)){
 					msg[i].setFlag(Flags.Flag.DELETED, true);
-					System.out.println("Msg Deleted\n");
+					System.out.println("Message Deleted\n");
 				}
 				else
-					System.out.println("Msg kept-proceeding to next msg\n");
+					System.out.println("Message kept-proceeding to next message\n");
+				
+				//If end of email list, notify user.
+				if(i==0)
+					System.out.println("End of messages");
          
-			}/**end of forloop**/
+			}/**end of forloop**/	
 			
-			//Close connection
+			
+			//Some POP3 servers will mark read messages for deletion. To prevent this from happening
+			//before the client deletes the messages themselves, issue POP3 RSET command.
+			//This clears all "marked for deletion" flags. We can then explicitly delete the messages ourselves.
+			prop.put("mail.pop3.rsetbeforequit", true);				
+			
+			//Close connection-By closing connection. All read messages (msg that have gone thru InputStream)
+			//will be deleted. To prevent this, do not close store and inbox.
+			/**
 			inbox.close(true);
 			store.close();       
-    
+    		**/
+			
 		}/**end of try block**/
     
 		catch (Exception e) {
@@ -217,7 +215,7 @@ public class dbworldintegration {
 	                stmt.close();
 	        }
 	        catch (Exception e) {
-	        	//TODO log "Cannot close Statement"
+	        	// log "Cannot close Statement"
 	        	System.out.println("Cannot close Statement : " + e.getMessage());
 	        }
 	        try {
@@ -225,13 +223,38 @@ public class dbworldintegration {
 	                conn.close();
 	        }
 	        catch (SQLException e) {
-	        	//TODO log Cannot close Connection
+	        	// log "Cannot close Connection"
 	        	System.out.println("Cannot close Connection : " + e.getMessage());
 	        }
 	    }
 		
 	}/**end of method receive(String username, String password)**/
+
 	
+/*
+ * checkHeaderForJobAnn(Enumeration headers)
+ * 
+ * This method checks through every header in the message to see if it contains
+ * "job/announcement". If yes, set flag to true.
+ *  
+ * @param: headers - list of all the headers in the message
+ */
+	protected boolean checkHeaderForJobAnn(Enumeration headers){
+		boolean flag = false;
+        int num = 0;				
+        while (headers.hasMoreElements()) {
+            Header h = (Header) headers.nextElement();
+            if(h.getValue().contains("job/announcement"))
+            {
+            	flag = true;
+            	System.out.println("job!");
+            	break;
+            }
+            num++;
+        }
+		return flag;
+	}
+
 	
 /*
  * createJobAdvertisementWithEmail(Statement stmt, String subject, String message, long millisSentDate)
@@ -244,8 +267,7 @@ public class dbworldintegration {
  * @param: millisSentDate - Date the email was sent in milliseconds. Also used to compute JobAdID	
  * 
  */
-	
-	public void createJobAdvertisementWithEmail(Statement stmt, String subject, String message, long millisSentDate /**,expiryDate**/){
+	protected void createJobAdvertisementWithEmail(Statement stmt, String subject, String message, long millisSentDate /**,expiryDate**/){
 		try{
 			int jobAdId = -1;
 			String jobAdvertisementTitle = Utility.checkInputFormat(subject);
@@ -253,15 +275,13 @@ public class dbworldintegration {
 		
 			//Add new entry with specified parameters into database
 			String query = 
-				"INSERT INTO tableJobAd(title, description, datePosted) " +
-				"VALUES " + "('" 
-				+ jobAdvertisementTitle  + "','" 
-				+ jobDescription  + "','" 
-				+ millisSentDate	  +
-				"')"; 
+				("INSERT INTO tableJobAd(title, description, datePosted) "
+						+ "VALUES " + "('" + jobAdvertisementTitle  + "','" 
+						+ jobDescription + "','" + millisSentDate + "')"); 
 		
 			//If successful, 1 row should be inserted
 			System.out.println("New Job Ad query: " + query);
+
 			int rowsInserted = stmt.executeUpdate(query);
 			
 			if(rowsInserted!= 1){
@@ -269,7 +289,7 @@ public class dbworldintegration {
 				Utility.logError("New JobAd insert in DB failed");
 			}
 
-		//Get jobAdId
+			//Get jobAdId
 			query = "SELECT idJobAd FROM tableJobAd WHERE " +
 				"title='" + jobAdvertisementTitle + "' AND " +
 				"datePosted='" + millisSentDate + "'";
@@ -289,49 +309,7 @@ public class dbworldintegration {
 			e.printStackTrace();
 		} 
 	}/**end of createJobAdvertismentWithEmail**/
-
-/*
- * searchDBWorldAnnPosting(NodeList trs, String msgSubject)
- * 
- * This method sees if given email message is a job announcement posting (job ann.) 
- * in DbWorld mailing list website. 
- * 
- * @param: NodeList trs - a list of nodes that contain html table rows
- * @param: String msgSubject - email message Subject Heading
- *
- */
-	public boolean searchDBWorldAnnPosting(NodeList trs, String msgSubject){
-		boolean flag=false;
-		try {
-			for(int i=1;i<=10;i++) {
-				//Filter through all <trs> and <tds> tags in html table
-				NodeList nodes = trs.elementAt(i).getChildren();
-				NodeList tds  = nodes.extractAllNodesThatMatch(new TagNameFilter("td"),true);
-				
-				//Get 2nd column in mailing list HTML table-Job Type
-				NodeList annNode = tds.elementAt(1).getChildren();
-				String annNodeToString = annNode.asString();
-				
-				//Get 4th column in mailing list HTML table-Posting Subject
-				NodeList subjectNode = tds.elementAt(3).getChildren();
-				String subjectNodeToString = subjectNode.asString();
-				
-				if(msgSubject.contains(subjectNodeToString) && annNodeToString.contains("job ann.")) 
-				{
-					flag=true;
-					break; //break out of function
-				}
-				else
-					flag=false;	
-			}/**end of for loop**/ 
-		}/**end of try block**/
 	
-		catch (Exception e) {
-			e.printStackTrace();
-		}    	
-		return flag;
-	}/**end of searchDBWorld()**/
-		
 }/**end of class dbworlintegration**/
 
 
