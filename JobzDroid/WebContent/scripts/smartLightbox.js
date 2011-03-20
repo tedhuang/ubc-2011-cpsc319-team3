c1020ea6a80c20242312f8fbdefc613efc8c2762
abigrabit <abigrabit@98e01662-c3e2-1594-295b-da2b039ece72>
2011-03-20 21:57:31 +0000

    Harry, update

---
+++ JobzDroid/WebContent/scripts/smartLightbox.js
@@ -0,0 +1,143 @@
+/*
+ * LightBox
+ * LightBox Plugin
+ * Author: Cheng Chen
+ * 
+ */
+(function($){
+    $.fn.smartLightBox = function(options) {
+       var options = $.extend({}, $.fn.smartLightBox.defaults, options);//keep default, only update from the "options"
+
+        return this.each(function() {
+        	
+        		lightBox = $(this);
+        		
+//        		divLightBox('loginbox','load','loading...');
+   /************************STARTOF FUNCTION GROUP******************************************************************************/
+        		function buildInfoBox(text, imgClass){
+             		var infoBox= $('<div></div>').attr('id', 'info');
+             		$('<img>').attr('id','lbImg').addClass(imgClass).appendTo(infoBox);
+             		$('<h2></h2>').attr('id','lbMsg').text(text).appendTo(infoBox);
+             		 return infoBox;
+             		 
+             	}
+        		
+        		function fullLightBox(lb_class, infoBox){
+     		    
+     			// hide scrollbars!
+     			$('body').css('overflow-y', 'hidden');
+     			
+     			$('<div id="overlay"></div>').addClass('lb_black')
+     			.css('opacity', '0').animate({'opacity': '0.5'}, 1000)
+//     			.hide()
+     			.appendTo(lightBox);
+     			
+     			$('<div></div>').attr('id','midBox')
+	 							.css('opacity', '0').animate({'opacity': '1'}, 1000)
+	 							.appendTo(lightBox);
+     			infoBox.appendTo('#midBox');
+     			
+     			lightBox
+     			.addClass('on')
+     			.css('top', $(document).scrollTop())
+     			.appendTo('body');
+     	}
+     	
+     	function smallLightBox(infoBox){
+     	// hide scrollbars!
+ 			$('body').css('overflow-y', 'hidden');
+ 			$('<div id="overlay"></div>').appendTo(lightBox);
+ 			$('<div></div>').attr('id','midBox').css('opacity', '0').animate({'opacity': '1'}, 1000).appendTo(lightBox);
+ 			infoBox.appendTo('#midBox');
+ 			lightBox.addClass('on').css('top', $(document).scrollTop()).appendTo('body');
+     	}
+     	
+     	function divLightBox(div,imgClass, msg){
+//     		$("#"+div).css('overflow-y', 'hidden');
+     		var lbClone=lightBox.clone();
+     		
+     		lbClone.addClass('on')
+     			   .css('top', $(document).scrollTop())
+     			   .appendTo("#"+div);
+     		
+     		$('<div id="overlay"></div>')
+	     		.addClass('lb_black')
+	 			.css('opacity', '0')
+	 			.animate({'opacity': '0.5'}, 1000)
+	 			.appendTo(lbClone);
+     		
+ 			$('<div></div>').attr('id','divlb').appendTo(lbClone);
+ 			$('<img/>')
+ 				.attr('id','lbImg')
+ 				.addClass(imgClass)
+ 				.appendTo($("#divlb","#"+div));
+ 			$('<h2></h2>')
+ 				.attr('id','lbMsg')
+ 				.text(msg).css('opacity', '0')
+ 				.animate({'opacity': '1'}, 1000)
+ 				.appendTo($("#divlb","#"+div));
+     	}
+     	
+     	function rmLightbox(dTime){
+ 			var time =(dTime==null?0:dTime);
+ 			$('#overlay, #midBox')
+ 			.delay(time)
+ 			.fadeOut('fast', function() {
+ 			$(this).remove();
+ 			$('body').css('overflow-y', 'auto'); // show scrollbars!
+ 			lightBox.removeClass('on');
+ 			});
+ 	};
+ 	
+ 	function rmDivlb(div, dTime){
+ 		   var time=(!dTime?0:dTime);
+			$('#overlay, #divlb', "#"+div)
+			.delay(time)
+			.fadeOut("fast", function() {
+//			$("#"+div).css('overflow-y', 'auto'); // show scrollbars!
+			$(this).remove();
+			$("#"+div).find("#lightBox").remove();
+			});
+	};
+     	
+     	$.fn.smartLightBox.openlb=function(mode, text, imgClass){
+     		switch (mode){
+     		
+     			case "full":
+     				fullLightBox(buildInfoBox(text,imgClass));
+     				break;
+     			case "small":
+     				smallLightBox(buildInfoBox(text,imgClass));
+     				break;
+//     			case "div":
+//     				divLightBox($(this),imgClass,text);
+//     				break;
+     			case "info":
+     				smallLightBox(buildInfoBox(text,imgClass));
+     	     		rmLightbox(2500);
+     			default:
+     				fullLightBox(buildInfoBox(text,imgClass));
+ 					break;
+     		}
+     	};
+     	
+     	
+     	$.fn.smartLightBox.closeLightBox=function(dTime,div ){
+ 			if(!div){ rmLightbox(dTime);}
+ 			else	{rmDivlb(div, dTime);}
+ 		};
+     	
+     	$.fn.smartLightBox.openDivlb=function(div, imgClass, msg){
+     		divLightBox(div, imgClass, msg);
+     	};
+     	
+     	
+  /************************ENDOF FUNCTION GROUP*********************************************/
+        });  // ENDOF return Each
+    };  //ENDOF smartLightBox
+ 
+    $.fn.smartLightBox.defaults = {
+    		lbClass: "on"
+    };
+
+})(jQuery);
