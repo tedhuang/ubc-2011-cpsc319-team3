10a4b6cd3ba5ca9d51a56495d0fa14509068cb79
tedhuang88@gmail.com <tedhuang88@gmail.com@98e01662-c3e2-1594-295b-da2b039ece72>
2011-03-27 20:24:33 +0000

    harry you committed an extra } in jobAd.js

--- JobzDroid/src/servlets/ServletJobAd.java
+++ JobzDroid/src/servlets/ServletJobAd.java
@@ -426,43 +426,42 @@ public class ServletJobAd extends HttpServlet {
 				jobAdList.add(jobAd);
 				
 			/**Get Employment Availability values */
-				stmtEmp = conn.createStatement();
-
+//				stmtEmp = conn.createStatement();
+//				String empAvail = "";
+//				int fullTime = 0;
+//				int partTime = 0;
+//				int internship = 0;
+//				
+//				query = "SELECT * FROM tableEmpTypeJobAd WHERE " +
+//				"idJobAd= '" + result.getInt("idJobAd") + "'";
+//				System.out.println(query);
+//				
+//				isSuccessful = stmtEmp.execute(query);
+//				ResultSet empResult = stmtEmp.getResultSet();
+//				
+//				if(!empResult.first()){
+//						System.out.println("No Employment Type Found");
+//				}
+//				else{
+//					fullTime = empResult.getInt("fullTime");
+//					partTime = empResult.getInt("partTime");
+//					internship = empResult.getInt("internship");	
+//
+//					if( (fullTime+partTime+internship) == 0 ){
+//						empAvail = "Not Specified"; //no preference
+//					}
+//					else{
+//						if( fullTime == 1)
+//							empAvail += "Full-time ";
+//						if(partTime == 1)
+//							empAvail += "Part-time ";
+//						if(internship == 1)
+//							empAvail += "Internship";
+//					}
+//					System.out.println("Job Positions Available: " + empAvail);
+//					jobAd.jobAvailability = empAvail;
+//				}
 				
-				String empAvail = "";
-				int fullTime = 0;
-				int partTime = 0;
-				int internship = 0;
-				
-				query = "SELECT * FROM tableEmpTypeJobAd WHERE " +
-				"idJobAd= '" + result.getInt("idJobAd") + "'";
-				System.out.println(query);
-				
-				isSuccessful = stmtEmp.execute(query);
-				ResultSet empResult = stmtEmp.getResultSet();
-				
-				if(!empResult.first()){
-						System.out.println("No Employment Type Found");
-				}
-				else{
-					fullTime = empResult.getInt("fullTime");
-					partTime = empResult.getInt("partTime");
-					internship = empResult.getInt("internship");	
-
-					if( (fullTime+partTime+internship) == 0 ){
-						empAvail = "Not Specified"; //no preference
-					}
-					else{
-						if( fullTime == 1)
-							empAvail += "Full-time ";
-						if(partTime == 1)
-							empAvail += "Part-time ";
-						if(internship == 1)
-							empAvail += "Internship";
-					}
-					System.out.println("Job Positions Available: " + empAvail);
-					jobAd.jobAvailability = empAvail;
-				}
 			}//END OF WHILE LOOP
 			
 			if(jobAdList.isEmpty()){
