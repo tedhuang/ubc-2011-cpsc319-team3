b066758960a1410a4bbc4d5e0193005ae7d9da4e
martinku86 <martinku86@98e01662-c3e2-1594-295b-da2b039ece72>
2011-04-02 22:25:40 +0000

    - Removed ServletHTTPS - Removed Test_HTTPS_Servlet - matched web.xml - Refactored forgetpassword to accountAction

--- trunk/JobzDroid/WebContent/scripts/accountAction.js
+++ trunk/JobzDroid/WebContent/scripts/accountAction.js
@@ -169,10 +169,57 @@ function submitChangeAccount(){
 }
 
 
+/*****************************************************************************************************
+ * 					Send Password Change - Send Change Passowrd Request
+ ****************************************************************************************************/
+function sendPasswordChangeRequest(evt){
+	$("#sendButton").attr("disabled", true);
+	$("#statusText").removeClass("errorTag");	
+	$("#statusText").removeClass("successTag");
+	var strEmail = $("#emailInput").val();
+	
+	var xmlHttpReq;
+	if (window.XMLHttpRequest){
+		// Firefox, Chrome, Opera, Safari
+		xmlHttpReq = new XMLHttpRequest();
+	}
+	else{
+		// IE
+		xmlHttpReq = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	  
+	xmlHttpReq.onreadystatechange = function(){
+		if (xmlHttpReq.readyState == 4 && xmlHttpReq.status == 200){
+			$("#sendButton").removeAttr("disabled");
+			//parse XML response from server
+			var responseText = parseForgetPasswordResponse(xmlHttpReq.responseXML);
+	    	$("#statusText").text(responseText);
+		}};
 
+	request = new Request;
+	request.addAction("requestForgetPassword");
+	request.addParam("email", strEmail);
 
+	//send the request to servlet
+	xmlHttpReq.open("POST","../ServletAccount", true);
+	xmlHttpReq.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xmlHttpReq.send(request.toString());
+	
+	//update status text
+	$("#statusText").text("Processing...This may take a moment.");
+}
 
-
+// parses response from server
+function parseForgetPasswordResponse(responseXML){	
+	 var boolResult = (responseXML.getElementsByTagName("result")[0]).childNodes[0].nodeValue;
+	 var strMsg = (responseXML.getElementsByTagName("message")[0]).childNodes[0].nodeValue;
+	 if(boolResult == "true"){
+		 $("#statusText").addClass("successTag");
+	 }
+	 else
+		 $("#statusText").addClass("errorTag");
+	 return strMsg;
+}
 
 
 
