cdd7674aefa323e1605d54459e978b3a9a02253e
luolw8026@gmail.com <luolw8026@gmail.com@98e01662-c3e2-1594-295b-da2b039ece72>
2011-04-03 00:21:49 +0000

    - servlet admin refactored shawn

--- JobzDroid/src/managers/AccountManager.java
+++ JobzDroid/src/managers/AccountManager.java
@@ -1000,6 +1000,147 @@ public class AccountManager {
 	    return false;
 	}
 	
+	/***
+	 * Bans the given account.
+	 * @param email Account name to ban.
+	 * @return boolean indicating whether the ban was successful.
+	 */
+	public boolean banAccount(String email){
+		Connection conn = dbManager.getConnection();
+		Statement stmt = null;
+		ResultSet rs = null;		
+		
+		try {
+			stmt = conn.createStatement();
+			String query = "UPDATE tableAccount SET status='banned' WHERE email='" + email + "';";
+            
+			int rowsInserted = stmt.executeUpdate(query);
+			// if successful, 1 row should be inserted
+			if (rowsInserted != 1)
+				return false;
+			
+			return true;
+		}
+		catch (SQLException e) {
+			Utility.logError("SQL exception: " + e.getMessage());
+			return false;	
+		}
+		// free DB objects
+	    finally {
+	        try {
+	            if (rs != null)
+	                rs.close();
+	        }
+	        catch (Exception e){
+	        	Utility.logError("Cannot close ResultSet: " + e.getMessage());
+	        }
+	        try{
+	            if (stmt != null)
+	                stmt.close();
+	        }
+	        catch (Exception e) {
+	        	Utility.logError("Cannot close Statement: " + e.getMessage());
+	        }
+	        dbManager.freeConnection(conn);
+	    }
+	}
+	
+	/***
+	 * Unbans the given account.
+	 * @param email Account name to unban.
+	 * @return boolean indicating whether the unban was successful.
+	 */
+	public boolean unbanAccount(String email){
+		Connection conn = dbManager.getConnection();
+		Statement stmt = null;
+		ResultSet rs = null;		
+		
+		try {
+			stmt = conn.createStatement();
+			String query = "UPDATE tableAccount SET status='active' WHERE email='" + email + "';";
+            
+			int rowsInserted = stmt.executeUpdate(query);
+			// if successful, 1 row should be inserted
+			if (rowsInserted != 1)
+				return false;
+			
+			return true;
+		}
+		catch (SQLException e) {
+			Utility.logError("SQL exception: " + e.getMessage());
+			return false;	
+		}
+		// free DB objects
+	    finally {
+	        try {
+	            if (rs != null)
+	                rs.close();
+	        }
+	        catch (Exception e){
+	        	Utility.logError("Cannot close ResultSet: " + e.getMessage());
+	        }
+	        try{
+	            if (stmt != null)
+	                stmt.close();
+	        }
+	        catch (Exception e) {
+	        	Utility.logError("Cannot close Statement: " + e.getMessage());
+	        }
+	        dbManager.freeConnection(conn);
+	    }
+	}
+	
+	/***
+	 * Creates an admin account with given account name and password.
+	 * @param email Account name to create.
+	 * @param password Password to use.
+	 * @return boolean indicating whether the admin creation was successful.
+	 */
+	public boolean createAdminAccount(String email, String password){
+		Connection conn = dbManager.getConnection();
+		PreparedStatement pst = null;
+		ResultSet rs = null;		
+		long currentTime = Utility.getCurrentTime();
+		try {
+			String query = "INSERT INTO tableAccount(email, password, type, status, dateTimeCreated)" +
+            		" VALUES(?,md5(?),'admin','active',?);";
+			
+			pst = conn.prepareStatement(query);
+			pst.setString(1, email);
+            pst.setString(2, password);
+            pst.setLong(3, currentTime);
+            
+			int rowsInserted = pst.executeUpdate();
+			// if successful, 1 row should be inserted
+			if (rowsInserted != 1)
+				return false;
+			
+			return true;
+		}
+		catch (SQLException e) {
+			Utility.logError("SQL exception: " + e.getMessage());
+			return false;	
+		}
+		// free DB objects
+	    finally {
+	        try {
+	            if (rs != null)
+	                rs.close();
+	        }
+	        catch (Exception e){
+	        	Utility.logError("Cannot close ResultSet: " + e.getMessage());
+	        }
+	        try{
+	            if (pst != null)
+	                pst.close();
+	        }
+	        catch (Exception e) {
+	        	Utility.logError("Cannot close Statement: " + e.getMessage());
+	        }
+	        dbManager.freeConnection(conn);
+	    }
+	}
+	
 	private boolean insertAddress(int idAccount, String address, String longitude, String latitude) {		
 		boolean isSuccessful = false;
 		
